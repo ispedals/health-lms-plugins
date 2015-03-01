91437bcc8e0c3755a8ac5a24420a01f93a7659d7
ispedals <ispedals@users.noreply.github.com>
2015-03-01 15:11:42 -0500

    start skeleton filter

---
+++ moodle-filter_viewerjs/filter.php
@@ -0,0 +1,8 @@
+<?php
+class filter_viewerjs extends moodle_text_filter {
+    public function filter($text, array $options = array()) {
+        global $CFG, $PAGE;
+        return $text;
+    }
+}
+?>
---
+++ moodle-filter_viewerjs/version.php
@@ -0,0 +1,7 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+$plugin->version   = 2015010800;            // The current plugin version (Date: YYYYMMDDXX)
+$plugin->maturity = MATURITY_STABLE;        // this version's maturity level.
+$plugin->release = '1.0';
+$plugin->requires  = 2014050800;           // Requires this Moodle version
+?>
