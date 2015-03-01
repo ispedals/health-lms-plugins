94caa92f8fae753dd02fc576147e066a8a8b27f8
ispedals <ispedals@users.noreply.github.com>
2015-03-01 16:12:02 -0500

    initial capture of pdf url

--- moodle-filter_viewerjs/filter.php
+++ moodle-filter_viewerjs/filter.php
@@ -1,8 +1,35 @@
 <?php
+
+defined('MOODLE_INTERNAL') || die();
+
+/*
+    adapted from filter_jwplayer  in filter.php trom
+    https://github.com/lucisgit/moodle-filter_jwplayerwhich in turn is adapted from
+    filter/mediaplugin/filter.php from the Moodle core
+
+    filter_jwplayer is released under GNU GPL v3
+*/
+
 class filter_viewerjs extends moodle_text_filter {
+
+
     public function filter($text, array $options = array()) {
         global $CFG, $PAGE;
-        return $text;
+
+        if (!is_string($text) or empty($text)) {
+            return $text;
+        }
+
+        if (stripos($text, '</a>') === false) {
+            return $text;
+        }
+
+        $newtext = preg_replace('/<a\s[^>]*href="([^"]*(?:[.]pdf)[^"]*)"[^>]*>([^>]*)<\/a>/is',"captured $1", $text);
+
+        if (empty($newtext) or $newtext === $text) {
+            return $text;
+        }
+        return $newtext;
     }
 }
 ?>
