ebfeecc00d9a4e40f9f8fc68ab79cb084a2bc11c
ispedals <ispedals@users.noreply.github.com>
2015-03-02 15:40:48 -0500

    copy code from filter_mediaplugin

--- moodle-filter_viewerjs/filter.php
+++ moodle-filter_viewerjs/filter.php
@@ -4,19 +4,20 @@ defined('MOODLE_INTERNAL') || die();
 require_once($CFG->dirroot.'/filter/viewerjs/lib.php');
 
 /*
-	adapted from filter_jwplayer  in filter.php trom
-	https://github.com/lucisgit/moodle-filter_jwplayer which in turn is adapted from
-	filter/mediaplugin/filter.php from the Moodle core
-
-	filter_jwplayer is released under GNU GPL v3
+	adapted from filter_mediaplugin in filter/mediaplugin/filter.php from the Moodle core
+	https://github.com/lucisgit/moodle-filter_jwplayer used for guidance
+    
+    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
 */
 
 class filter_viewerjs extends moodle_text_filter {
-	protected $renderer;
-	protected $embedmarkers;
+	private $mediarenderer;
 
 
-	public function filter($text, array $options = array()) {
+	// copied from filter_mediaplugin::filter
+    // the reason we are not using inheritance is because
+    // filter_mediaplugin::mediarenderer is private and we have different logic in how it is set
+    public function filter($text, array $options = array()) {
 		global $PAGE;
 
 		if (!is_string($text) or empty($text)) {
@@ -27,39 +28,74 @@ class filter_viewerjs extends moodle_text_filter {
 			return $text;
 		}
 
-		if (!$this->renderer) {
-			$this->renderer = $PAGE->get_renderer('filter_viewerjs');
-			$this->embedmarkers = $this->renderer->get_embeddable_markers();
+		if (!$this->mediarenderer) {
+			$this->mediarenderer = $PAGE->get_renderer('filter_viewerjs');
+			$embedmarkers = $this->mediarenderer->get_embeddable_markers();
 		}
 
-		//matches href and tag contents
-		//TODO see if we can use a real parser to protect against potential security problems
-		$newtext = preg_replace_callback('/<a\s[^>]*href="([^"]*(?:' . $this->embedmarkers . ')[^"]*)"[^>]*>([^>]*)<\/a>/is', array($this, 'callback'), $text);
-
-		if (empty($newtext) or $newtext === $text) {
-			return $text;
-		}
-		return $newtext;
+        // Looking for tags.
+        $matches = preg_split('/(<[^>]*>)/i', $text, -1, PREG_SPLIT_NO_EMPTY | PREG_SPLIT_DELIM_CAPTURE);
+        
+        if (!$matches) {
+            return $text;
+        }
+        
+        // Regex to find media extensions in an <a> tag.
+        $re = '~<a\s[^>]*href="([^"]*(?:' .  $embedmarkers . ')[^"]*)"[^>]*>([^>]*)</a>~is';
+        $newtext = '';
+        $validtag = '';
+        $sizeofmatches = count($matches);
+        
+        // We iterate through the given string to find valid <a> tags
+        // and build them so that the callback function can check it for
+        // embedded content. Then we rebuild the string.
+        foreach ($matches as $idx => $tag) {
+            if (preg_match('|</a>|', $tag) && !empty($validtag)) {
+                $validtag .= $tag;
+                // Given we now have a valid <a> tag to process it's time for
+                // ReDoS protection. Stop processing if a word is too large.
+                if (strlen($validtag) < 4096) {
+                    $processed = preg_replace_callback($re, array($this, 'callback'), $validtag);
+                }
+                // Rebuilding the string with our new processed text.
+                $newtext .= !empty($processed) ? $processed : $validtag;
+                // Wipe it so we can catch any more instances to filter.
+                $validtag = '';
+                $processed = '';
+            } else if (preg_match('/<a\s[^>]*/', $tag) && $sizeofmatches > 1) {
+                // Looking for a starting <a> tag.
+                $validtag = $tag;
+            } else {
+                // If we have a validtag add to that to process later,
+                // else add straight onto our newtext string.
+                if (!empty($validtag)) {
+                    $validtag .= $tag;
+                } else {
+                    $newtext .= $tag;
+                }
+            }
+        }
+        // Return the same string except processed by the above.
+        return $newtext;
 	}
 
 	private function callback(array $matches) {
-		// Get name.
-		$name = trim($matches[2]);
-		 if (empty($name) or strpos($name, 'http') === 0) {
-			$name = '';
-		}
+        // Get name.
+        $name = trim($matches[2]);
+        if (empty($name) or strpos($name, 'http') === 0) {
+            $name = ''; // Use default name.
+        }
 
 		// Split provided URL into alternatives.
 		$urls = core_media::split_alternatives($matches[1], $width, $height);
-		$result = $this->renderer->embed_alternatives($urls, $name, $width, $height);
+		$result = $this->mediarenderer->embed_alternatives($urls, $name, $width, $height);
 
-		// If something was embedded, return it, otherwise return original.
-		if ($result !== '') {
-			return $result;
-		}
-		else {
-			return $matches[0];
-		}
+        // If something was embedded, return it, otherwise return original.
+        if ($result !== '') {
+            return $result;
+        } else {
+            return $matches[0];
+        }
 	}
 }
 ?>
