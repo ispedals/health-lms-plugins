da88331c14e744dd62619a26abee8686717adde8
ispedals <ispedals@users.noreply.github.com>
2015-03-17 15:07:40 -0400

    Only store final grades
    This plugin will now act as a repository for final grades. We will use
    configurable reports to get current grades so that the information can
    be merged into a single view

--- moodle-local_tincanevents/db/events.php
+++ moodle-local_tincanevents/db/events.php
@@ -2,16 +2,8 @@
 
 $observers = array(
 	array (
-	    'eventname'     => '\mod_quiz\event\attempt_submitted',
-	    'callback' => 'report_tincan_observer::tincan_quiz_attempt_submitted',
-	),
-	array (
-	    'eventname'     => '\mod_quiz\event\attempt_started',
-	    'callback' => 'report_tincan_observer::tincan_quiz_attempt_started',
-	),
-	array (
 	    'eventname'     => '\core\event\course_completed',
 	    'callback' => 'report_tincan_observer::tincan_course_completed',
-	),
+	)
 );
 ?>
